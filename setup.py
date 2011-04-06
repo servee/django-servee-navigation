4c5b50c30370d2d4d4707ff032f514fd23a1572f
Issac Kelly <issac.kelly@gmail.com>
2011-04-06 10:30:04 -0400

    fix setup.py

--- setup.py
+++ setup.py
@@ -24,33 +24,6 @@ def find_package_data(
     exclude_directories=standard_exclude_directories,
     only_in_packages=True,
     show_ignored=False):
-    """
-    Return a dictionary suitable for use in ``package_data``
-    in a distutils ``setup.py`` file.
-
-    The dictionary looks like::
-
-        {'package': [files]}
-
-    Where ``files`` is a list of all the files in that package that
-    don't match anything in ``exclude``.
-
-    If ``only_in_packages`` is true, then top-level directories that
-    are not packages won't be included (but directories under packages
-    will).
-
-    Directories matching any pattern in ``exclude_directories`` will
-    be ignored; by default directories with leading ``.``, ``CVS``,
-    and ``_darcs`` will be ignored.
-
-    If ``show_ignored`` is true, then all the files that aren't
-    included in package data are shown on stderr (for debugging
-    purposes).
-
-    Note patterns use wildcards, or can be exact paths (including
-    leading ``./``), and all searching is case-insensitive.
-    """
-
     out = {}
     stack = [(convert_path(where), '', package, only_in_packages)]
     while stack:
@@ -110,7 +83,8 @@ setup(
     author = 'Josh Boles',
     author_email = 'josh@servee.com',
     packages = find_packages(exclude=["example_project*",]),
-    package_data = find_package_data('servee_navigation',only_in_packages=False),
+    package_data = find_package_data("servee_navigation", only_in_packages=False),
+    include_package_data=True,
     install_requires = [
         'django-servee>0.5.5',
         'django-treebeard',
